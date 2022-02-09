fe070a01449621f13810a5c985d69c226934310c
citikot <citikot@dr.com>
2022-02-09 23:07:00 +0200

    tests example added

--- task12/script.js
+++ task12/script.js
@@ -40,3 +40,65 @@ function createParagraph(text) {
     par.textContent = text;
     return par;
 }
+
+/*
+
+export function smartForm(el) {
+  const NUMBER_OF_PARAGRAPHS = 3;
+
+  let input = document.createElement("input");
+  el.appendChild(input);
+
+  let button = document.createElement("button");
+  el.appendChild(button);
+
+  let par = [];
+
+  for (let i = 0; i < NUMBER_OF_PARAGRAPHS; i++) {
+    par[i] = document.createElement("p");
+    el.appendChild(par[i]);
+  }
+
+  button.addEventListener("click", () => {
+    par.push(document.createElement("p"));
+    par[par.length - 1].innerHTML = input.value;
+    el.appendChild(par[par.length - 1]);
+  });
+}
+
+
+smartForm.test.js
+
+import { smartForm } from "./smartForm";
+
+describe("smartForm", () => {
+  let el;
+  beforeEach(() => {
+    el = document.createElement("div");
+    smartForm(el);
+  });
+
+  function submitValue(text) {
+    el.querySelector("input").value = text;
+    el.querySelector("button").click();
+  }
+
+  it("is a function", () => {
+    expect(smartForm).toBeInstanceOf(Function);
+  });
+
+  it("creates initial markup", () => {
+    expect(el.querySelector("input")).toBeTruthy();
+    expect(el.querySelector("button")).toBeTruthy();
+    expect(el.querySelectorAll("p").length).toBe(3);
+  });
+
+  it("adds paragraph on button click", () => {
+    const value = "123";
+    submitValue(value);
+    expect(el.querySelectorAll("p").length).toBe(4);
+    expect(el.querySelectorAll("p")[3].innerHTML).toBe(value);
+  });
+});
+
+*/
